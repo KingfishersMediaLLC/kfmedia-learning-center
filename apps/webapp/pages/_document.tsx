b663ec434b2a7f2e28dacf611c356a0ca8ae2cc6
KingfishersMediaLLC <kingfishersmediallc@kingfishersmedia.io>
2026-04-19 14:49:11 -0600

    Add favicon and update theme color to KFMEDIA red

--- apps/webapp/pages/_document.tsx
+++ apps/webapp/pages/_document.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v15-pagesRouter';
-import { charmBlue as blueColor } from '@packages/config/colors';
+import { primaryBrandColor } from '@packages/config/colors';
 import { Head, Html, Main, NextScript } from 'next/document';
+
+const brandColor = '#903235';
 
 export default function MyDocument(props: any) {
   return (
     <Html lang='en'>
       <Head>
         <DocumentHeadTags {...props} />
-        <meta name='theme-color' content={blueColor} />
+        <meta name='theme-color' content={brandColor} />
         <link rel='icon' href='/favicon.png' />
         <script src='/__ENV.js' />
       </Head>
